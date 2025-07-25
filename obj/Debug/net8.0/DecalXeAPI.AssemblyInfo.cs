bf557007c13b4460ed468286aa2703b9aceecadc
Mai Nguyễn Tuấn Tú <tuantumn.0103@gmail.com>
2025-07-25 13:25:43 +0700

    feats

--- obj/Debug/net8.0/DecalXeAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/DecalXeAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DecalXeAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+769f6df3f798d60246385f952c8b448b356a5084")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2b743ceb5dcc1ba6bf2854c0e07aedce56f8302")]
 [assembly: System.Reflection.AssemblyProductAttribute("DecalXeAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DecalXeAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
