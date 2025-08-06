82ebeb4c3f2e4c5914b87a069c14726a39b81468
mainguyentuantu <whoiam.10322@gmail.com>
2025-08-06 11:03:49 +0700

    changing to page size

--- obj/Debug/net8.0/DecalXeAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/DecalXeAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DecalXeAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b097e093acd5e40df3cac8b9a1c2f5c79234e8ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+460d1da9c1633ebbfcd7f1f0daedd730b6160e16")]
 [assembly: System.Reflection.AssemblyProductAttribute("DecalXeAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DecalXeAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
