c187d46f966171578c8dfb11f4d8fb4daf6f035d
mainguyentuantu <whoiam.10322@gmail.com>
2025-08-06 09:11:42 +0700

    feat: Update logic in useVehicles hook

--- obj/Debug/net8.0/DecalXeAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/DecalXeAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DecalXeAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6c1745dda6577cee233a7884821f1adc8ea90fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b097e093acd5e40df3cac8b9a1c2f5c79234e8ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("DecalXeAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DecalXeAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
